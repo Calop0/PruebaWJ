780ae8739ace1a361482e97abe06a2df200fb06d
Calop0 <43230746+Calop0@users.noreply.github.com>
2019-02-12 17:40:24 -0500

    Delete codigo_edad_tema.py

--- codigo_edad_tema.py
+++
@@ -1,9 +0,0 @@
-import altair as alt
-import pandas as pd
-
-# Altair plots render by default in JupyterLab and nteract
-# Uncomment/run this line to enable Altair in the classic notebook (not in JupyterLab)
-a= alt.renderers.enable('notebook')
-
-blogs = pd.read_csv('blogtext.csv') # Importa los datos a un dataframe
-blogs = blogs.drop_duplicates(['id'], keep='first') # Se remueven id duplicados para no contar la misma persona dos veces
